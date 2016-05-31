bb3376cb474ce95a48f57de0e6cc7169657d7c71
Vlad <Vlad@MAH_EPIC_LAPTOP>
2016-05-31 16:13:54 +0300

    Add project files.

---
+++ PregatireExamenLPOO.cpp
@@ -0,0 +1,17 @@
+// Aplicatia va fi capabila sa faca cateva operatii simple cu vectori (si componentele lor - originea si varful).
+// Colectia de fisiere reprezinta un exemplu banal de folosire a claselor si obiectelor.
+
+#include "stdafx.h"
+
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+	
+
+	getchar();
+    return 0;
+}
+
